1c8c5e1c74d6d819036d1f862001236b3ec31577
Pablo Rodríguez <is.pab.ces@gmail.com>
2019-01-09 00:39:55 -0600

    dscript updated with CV tools
    dscript updated with CV tools preparation tools for PillowJ, ImageJ & OpenCV

--- dscript.sh
+++ dscript.sh
@@ -10,39 +10,53 @@
 #              Azure DSVM Ubuntu
 
 function update(){
-	apt-get clean
-	apt-get update -y
-	apt-get upgrade -y
-	apt-get dist-upgrade -y
+	echo ".:: UPDATING & UPGRADING ::."
+	apt clean
+	apt update -y
+	apt upgrade -y
+	apt dist-upgrade -y
 }
 
 function install_essentials(){
-	echo "INSTALING ESSENTIALS ..."
+	echo ".:: INSTALING ESSENTIALS ::."
 	apt install gnome-tweak-tool ubuntu-restricted-extras libavcodec-extra bleachbit plank inkscape gimp gimp-data gimp-plugin-registry gimp-data-extras dia  unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract gcp vlc -y
-	#echo "alias cp='gcp'" >> $HOME/.bashrc
-	nautilus -q
 }
 
 function install_chrome() {
+	echo ".:: CHROME INSTALLING ::."
 	if [[ $(getconf LONG_BIT) = "64" ]]
 	then
 		echo "64 bit Detected" 
 		echo "Installing Google Chrome" 
 		wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 		dpkg -i google-chrome-stable_current_amd64.deb 
-		#rm -f google-chrome-stable_current_amd64.deb
+		rm -f google-chrome-stable_current_amd64.deb
 	else
 		echo "32 bit Detected"
 		echo "Not supported any more! :(" 
 	fi
+	apt update
 } # install_chrome()
 
 
-function install_dev_tools(){
+function install_dev_tools() {
+	echo ".:: INSTALLING DEV TOOLS ::."
 	add-apt-repository ppa:webupd8team/java
 	apt update
 	apt install shellcheck mcedit mono-mcs gpp codeblocks build-essential octave
 	apt install oracle-java8-installer
+	update()
+}
+
+function install_cvtools() {
+	echo ".:: INSTALLING CV Tools ::."
+	apt install build-essential cmake unzip pkg-config
+	apt install libjpeg-dev libpng-dev libtiff-dev
+	apt install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+	apt install libxvidcore-dev libx264-dev
+	apt install libgtk-3-dev
+	apt install libatlas-base-dev gfortran
+	apt install python3-dev
 }
 
 while true ; do	
@@ -50,7 +64,7 @@ while true ; do
 	echo ".:: Script to Upgrade Ubuntu 18.0N.0N into a DSM ::."
 	echo "1. Update/upgrade 			2. Install Essentials"
 	echo "3. Install Chrome 			4. Install Dev Tools"
-	echo "5. Install IDEs 			6. Install DS Tools"
+	echo "5. Install CV Tools 			6. Install DS Tools"
 	echo "7. Install DIP Tools 			X. Exit"
 	echo -n "[x]> "
 	read -r OPTION
