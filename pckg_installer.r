5701aff78800da215c590978fe13ad5c6167dfd4
Pablo Rodríguez <is.pab.ces@gmail.com>
2019-01-24 22:50:03 -0600

    install_github update method
    install_github update method, seems like this package isn't available in R 3.5.2

--- pckg_installer.r
+++ pckg_installer.r
@@ -1,63 +1,79 @@
 install_opts <- "--byte-compile"
 reinstall <- FALSE
 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 # Installation functions
 installed <- data.frame(installed.packages())$Package
+
 # Get the bioconductor from source
 source("http://bioconductor.org/biocLite.R")
-
+ow
+# This function verifies the installation of the package.
 is.installed <- function(package.name) package.name %in% installed
 
 pastemsg <- function(...) message(paste(...))
+
+
 installer <- function(install.fn, ...) {
-  do.install <- function(package.name, check.installed=!reinstall) {
-    if(check.installed && is.installed(package.name)) {
-      pastemsg("Package", package.name, "is installed.")
-    } else {
-      pastemsg("Installing", package.name)
-      install.fn(c(package.name), ...)
-    }
-  }
+	do.install <- function(package.name, check.installed=!reinstall) {
+		if(check.installed && is.installed(package.name)) {
+			pastemsg("Package", package.name, "is installed.")
+		} else {
+			pastemsg("Installing", package.name)
+			install.fn(c(package.name), ...)
+		}
+	}
 }
 
 # Special installer for CRAN
-install_cran <- installer(install.packages, repos="http://cran.fhcrc.org",
-                          clean=TRUE, dependencies=TRUE,
-                          INSTALL_opts=install_opts)
+install_cran <- installer(install.packages, repos="http://cran.fhcrc.org", clean=TRUE, dependencies=TRUE, INSTALL_opts=install_opts)
 
 # Special installer for bioconductor
 install_bioconductor <- installer(biocLite, ask=FALSE, INSTALL_opts=install_opts)
 
-## Bioconductor Packages
-bioconductor_ <- paste(readLines('bioconductor_packages.txt'), sep = "\n")
-if(length(bioconductor_)){
-  library(BiocInstaller)
-  bioconductor_packages <- bioconductor_[!(bioconductor_ %in% installed.packages()[, "Package"])]
-  for(p in bioconductor_packages) {
-    tryCatch(install_bioconductor(p), error=warning)
-  }
+## Bioconductor Packages Installation 
+
+bioconductor_ <- paste(readLines('bioconductor_pckgs.txt'), sep = "\n")
+
+if(length(bioconductor_)) {
+	library(BiocInstaller)
+	bioconductor_pckgs <- bioconductor_[!(bioconductor_ %in% installed.packages()[, "Package"])]
+	for(pckg in bioconductor_pckgs) {
+		tryCatch(install_bioconductor(pckg), error=warning)
+	}
 }
+
 
 ## CRAN Packages
-cran_ <- paste(readLines('cran_packages.txt'), sep = "\n")
-if(length(cran_)){
-  print("Installing Packages from Cran")
-  cran_packages <- cran_[!(cran_ %in% installed.packages()[, "Package"])]
-  for(p in cran_packages) {
-    tryCatch(install_cran(p), error=warning)
-  }
+cran_ <- paste(readLines('cran_pckgs.txt'), sep = "\n")
+
+if(length(cran_)) {
+	print("Installing Packages from Cran")
+	cran_pckgs <- cran_[!(cran_ %in% installed.packages()[, "Package"])]
+	for(pckg in cran_pckgs) {
+		tryCatch(install_cran(pckg), error=warning)
+	}
 }
 
 
-## GitHub Package
-github_ <- paste(readLines('github_packages.txt'), sep="\n")
-if(length(github_)){
-  print("Installing Packages from GitHub")
-  github_packages <- github_[!(github_ %in% installed.packages()[, "Package"])]
-  for(p in github_packages) {
-    pastemsg("Installing", p, "from GitHub")
-    p <- list(p)
-    p$args <- install_opts
-    tryCatch(do.call('install_github', p), error=warning)
-  }
+## GitHub Packages 
+## NOTE: This method only works on versions < 3.5.2
+if ("install_github" %in% rownames(installed.packages()) == FALSE ) {
+	print("Installing: 'install_github'")
+	install.packages('install_github')
+} else {
+	print("'install_github' already installed...")
+}
+
+github_ <- paste(readLines('github_pckgs.txt'), sep="\n")
+if (length(github_)) {
+	print("Installing Packages from GitHub")
+	github_pckgs <- github_[!(github_ %in% installed.packages()[, "Package"])]
+	for(pckg in github_pckgs) {
+		pastemsg("Installing", pckg, "from GitHub")
+		pckg <- list(pckg)
+		pckg$args <- install_opts
+		tryCatch(do.call('install_github', pckg), error=warning)
+	}
 }
